4f5ebb2c9ffdbc713ccaeca83232b55025e0ff2c
andre <chang432@purdue.edu>
2018-09-26 10:09:38 -0400

    basic setup

--- __init__.py
+++ __init__.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('main.html');
-
+    #return render_template('main.html');
+    return "<h2>First Main Deploy!</h2>"
+    
 if __name__ == '__main__':
     app.run(debug=True)
