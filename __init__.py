a115dc6a2fcff33cc6459aa82753d42b97729395
Annie <anniex325@gmail.com>
2018-09-27 16:02:15 -0400

    add google button and page

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('main.html');
+    return render_template('main.html')
+
+@app.route('/facebook-google')
+def fglogin():
+    return render_template('facebook-google.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
