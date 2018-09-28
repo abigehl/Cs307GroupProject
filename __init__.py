0b4ea770e19b32733b57e9ba65929bc7ec9ac1de
Guillermo del Aguila <gdelagui@purdue.edu>
2018-09-27 21:32:34 -0400

    test hey commit

--- __init__.py
+++ __init__.py
@@ -2,13 +2,18 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('main.html')
+
 
 @app.route('/facebook-google')
 def fglogin():
     return render_template('facebook-google.html')
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+hey
