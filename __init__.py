7c62f03b1266a7f171bfd3644ea16899a1f7ddfd
andre <chang432@purdue.edu>
2018-09-27 21:30:51 -0400

    committing extra stuff

--- __init__.py
+++ __init__.py
@@ -4,17 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-<<<<<<< HEAD
-    return render_template('main.html');
-    #return "<h2>First Main Deploy!</h2>"
-    
-=======
+
     return render_template('main.html')
 
 @app.route('/facebook-google')
 def fglogin():
     return render_template('facebook-google.html')
 
->>>>>>> a115dc6a2fcff33cc6459aa82753d42b97729395
 if __name__ == '__main__':
     app.run(debug=True)
