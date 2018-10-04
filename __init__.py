b431de3605cadc2084b9a9bf7e9e887a8ecf4732
Guillermo del Aguila <gdelagui@purdue.edu>
2018-10-04 11:32:15 -0400

    main.html and init.py modifications to validate user login

--- __init__.py
+++ __init__.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 from flask_dance.contrib.google import make_google_blueprint, google
 from oauthlib.oauth2.rfc6749.errors import InvalidClientIdError
 import os
+
+
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 app = Flask(__name__)
+Bootstrap(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://helpmerecipe:passpass@helpmerecipe.coy90uyod5ue.us-east-2.rds.amazonaws.com/helpmerecipe'
 db = SQLAlchemy(app)
@@ -21,39 +28,58 @@ blueprint = make_google_blueprint(
     ],
     offline=True,
 )
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=30)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    remember = BooleanField('remember me')
+
+
 class users(db.Model):
-    name = db.Column(db.Unicode, primary_key = True)
+    name = db.Column(db.Unicode, primary_key=True)
     email = db.Column(db.Unicode)
 
+
 app.register_blueprint(blueprint, url_prefix="/login")
-@app.route('/',methods=['GET','POST'])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
 
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    #################################################
+    form = LoginForm()
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
+    #################################################
 
-        post = users(name=username,email=password)
+    # if request.method == 'POST':
+      #  username = request.form['username']
+       # password = request.form['password']
 
-        db.session.add(post)
-        db.session.commit()
+       # post = users(name=username, email=password)
 
-    return render_template('main.html')
+       # db.session.add(post)
+        # db.session.commit()
+
+    return render_template('main.html', form=form)
+
 
 @app.route('/facebook-google')
 def fglogin():
     return render_template('facebook-google.html')
 
-@app.route('/googleSignin', methods=['GET','POST'])
+
+@app.route('/googleSignin', methods=['GET', 'POST'])
 def googleSignin():
-    #print(session)
+    # print(session)
     if not google.authorized:
         return redirect(url_for("google.login"))
     try:
-        #print(session)
+        # print(session)
         resp = google.get("/oauth2/v2/userinfo")
         assert resp.ok, resp.text
-        post = users(name=resp.json()["name"], email=resp.json()["email"])#(name="Annie", email="something@gmail")
+        post = users(name=resp.json()["name"], email=resp.json()["email"])  # (name="Annie", email="something@gmail")
         print(post)
         db.session.add(post)
         db.session.commit()
@@ -62,6 +88,7 @@ def googleSignin():
         return render_template('main.html')
 
     return render_template('facebook-google.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
