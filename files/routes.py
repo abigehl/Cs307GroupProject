aa9a8b65a23a352bc95d30d3d8344085ed939ca9
andre <chang432@purdue.edu>
2018-10-28 02:20:43 -0400

    asdf

--- files/routes.py
+++ files/routes.py
@@ -1,4 +1,4 @@
-import secrets
+#import secrets
 import os
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from files import app, db, bcrypt, mail
@@ -43,11 +43,11 @@ def send_reset_email(user):
     msg = Message('Password reset Request',
                   sender='helpmerecipe@gmail.com',
                   recipients=[user.email])
-    msg.body = f'''To reset your password, visit the following link:
-{url_for('reset_token', token=token, _external=True)}
+#    msg.body = f'''To reset your password, visit the following link:
+#{url_for('reset_token', token=token, _external=True)}
 
-If you did not make this request then simply ignore this email and no changes will be made.
-'''
+#If you did not make this request then simply ignore this email and no changes will be made.
+#'''
     mail.send(msg)
 
 
